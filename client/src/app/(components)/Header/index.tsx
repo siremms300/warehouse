745d5e8eb53d6c5adf3e9e9e29e32d60a92be47c
Idoga Emmanuel <siremms300@gmail.com>
2025-02-28 16:40:35 -0800

    modified the header prop to include className

--- client/src/app/(components)/Header/index.tsx
+++ client/src/app/(components)/Header/index.tsx
@@ -1,9 +1,37 @@
+
 type HeaderProps = {
   name: string;
+  className?: string; // Add className as an optional prop
 };
 
-const Header = ({ name }: HeaderProps) => {
-  return <h1 className="text-2xl font-semibold text-gray-700">{name}</h1>;
+const Header = ({ name, className }: HeaderProps) => {
+  return (
+    <h1 className={`text-2xl font-semibold text-gray-700 ${className}`}>
+      {name}
+    </h1>
+  );
 };
 
 export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// type HeaderProps = {
+//   name: string;
+// };
+
+// const Header = ({ name }: HeaderProps) => {
+//   return <h1 className="text-2xl font-semibold text-gray-700">{name}</h1>;
+// };
+
+// export default Header;
